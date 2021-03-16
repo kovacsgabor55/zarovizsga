af076375a16cde1620ed6f9003e3123705bac375
ShadowDragon <kovacsgabor55@gmail.com>
2021-03-16 19:10:09 +0100

    hu.nive.ujratervezes.zarovizsga.dogtypes

---
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/dogtypes/DogTypes.java
@@ -0,0 +1,47 @@
+package hu.nive.ujratervezes.zarovizsga.dogtypes;
+
+import org.mariadb.jdbc.MariaDbDataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DogTypes {
+    private final MariaDbDataSource dataSource;
+
+    public DogTypes(MariaDbDataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public List<String> getDogsByCountry(String country) {
+        try (
+                Connection conn = dataSource.getConnection();
+                PreparedStatement stmt =
+                        conn.prepareStatement("SELECT `name` FROM `dog_types` WHERE `country` = ? ORDER BY `name` ASC;")
+        ) {
+            stmt.setString(1, country.toUpperCase());
+            stmt.executeUpdate();
+            return executeQuery(stmt);
+        } catch (SQLException sqle) {
+            throw new IllegalArgumentException("Error by select", sqle);
+        }
+    }
+
+    private List<String> executeQuery(PreparedStatement stmt) {
+        List<String> result = new ArrayList<>();
+        try (
+                ResultSet rs = stmt.executeQuery()
+        ) {
+            while (rs.next()) {
+                String name = rs.getString("name");
+                result.add(name.toLowerCase());
+            }
+            return result;
+        } catch (SQLException sqle) {
+            throw new IllegalArgumentException("Error by select", sqle);
+        }
+    }
+}
